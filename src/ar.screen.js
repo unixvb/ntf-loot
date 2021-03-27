7c73d92d3b696328ff024ec88f1c3b3dc5f742ff
Andrii Ivaniv <unixvb@gmail.com>
2021-03-27 16:51:38 +0200

    redundant files removed

--- src/ar.screen.js
+++ src/ar.screen.js
@@ -10,19 +10,12 @@ import {
   ViroSpotLight
 } from 'react-viro';
 
-const sharedResources = [
-  require('./assets/tex/black.png'),
-  require('./assets/tex/blue.png'),
-  require('./assets/tex/green.png'),
-  require('./assets/tex/pink.png'),
-];
-
 const scaleSize = .005;
 
 const sharedProps = {
   scale: [scaleSize, scaleSize, scaleSize],
-  position: [0, -0.2, -0.5],
-  type: "OBJ"
+  position: [0, -0.5, -1],
+  type: "GLB"
 };
 
 export default class ArScreen extends Component {
@@ -45,51 +38,12 @@ export default class ArScreen extends Component {
                        direction={[0, -1, -.2]}
                        position={[0, 3, 1]}
                        color="#ffffff"
+                       intensity={100}
                        castsShadow={true} />
 
         <Viro3DObject
+          source={{ uri: 'https://gateway.pinata.cloud/ipfs/QmXvxGf3WZvPJfXUAE4SNvwrPzAmwS2W6dP1EdGHBGV9ge/body.glb' }}
           {...sharedProps}
-          source={require(`./assets/body.obj`)}
-          resources={[
-            require(`./assets/body.mtl`),
-            ...sharedResources
-          ]}
-        />
-
-        <Viro3DObject
-          {...sharedProps}
-          source={require(`./assets/hat.obj`)}
-          resources={[
-            require(`./assets/hat.mtl`),
-            ...sharedResources
-          ]}
-        />
-
-        <Viro3DObject
-          {...sharedProps}
-          source={require(`./assets/eye.obj`)}
-          resources={[
-            require(`./assets/eye.mtl`),
-            ...sharedResources
-          ]}
-        />
-
-        <Viro3DObject
-          {...sharedProps}
-          source={require(`./assets/eyebrow_evil.obj`)}
-          resources={[
-            require(`./assets/eyebrow_evil.mtl`),
-            ...sharedResources
-          ]}
-        />
-
-        <Viro3DObject
-          {...sharedProps}
-          source={require(`./assets/mouth_smile.obj`)}
-          resources={[
-            require(`./assets/mouth_smile.mtl`),
-            ...sharedResources
-          ]}
         />
       </ViroARScene>
     );
